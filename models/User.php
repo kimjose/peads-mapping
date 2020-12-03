77169454abdeb4d5c784b1b08a485290f8b91d36
Jemimah Kinyua <jemkiyua@gmail.com>
2020-12-03 09:31:48 +0300

    users table added

---
+++ models/User.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . "/../bootstrap.php";
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model {
+    public $table = 'users';
+
+    protected $fillable = ['username','firstName','surname','cadre', 'facility','active'];
+}
--- panel/datascript.php
+++ panel/datascript.php
@@ -2,17 +2,20 @@
 
 require_once "functions.php";
 require_once "../models/OTZModules.php";
+require_once "../models/User.php";
 
 $request = $_GET['request'];
 $response = [];
 
 try {
 
-if ($request == "get_otz_modules") {
-    require_once "../models/OTZModules.php";
-    $modules = OTZModules::all();
-    echo myJsonResponse(200, "Modules retrieved", $modules);
-}
+    if ($request == "get_otz_modules") {
+        $modules = OTZModules::all();
+        echo myJsonResponse(200, "Modules retrieved", $modules);
+    } else if ($request == "get_users") {
+        $users = User::all();
+        echo myJsonResponse(200, "Users retrieved", $users);
+    }
 
 } catch (\Throwable $th) {
     echo myJsonResponse(400, $th->getMessage());
