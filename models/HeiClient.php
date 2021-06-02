cc1a5af25dd91f45a398ce1e091f55726f3a594e
kimjose <kimjose693@gmail.com>
2021-06-02 15:49:40 +0300

    HEI Shell started

---
+++ models/HeiClient.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace models;
+
+
+class HeiClient extends \Illuminate\Database\Eloquent\Model
+{
+    protected $table = 'hei_clients';
+
+    protected $fillable = ['hei_number', 'facility_code', 'dob', 'gender', 'name', 'status', 'status_date',
+        'pmtct_enrolled', 'pmtct_enrollment_date'];
+
+}
